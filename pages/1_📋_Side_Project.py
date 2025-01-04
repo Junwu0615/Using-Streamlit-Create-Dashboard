bf028910ba33664b3bda4ab1e9b1d282c30111cf
jun.audis5 <jun.audis5@gmail.com>
2025-01-04 20:57:43 +0800

    bulid detail content

--- pages/1_📋_Side_Project.py
+++ pages/1_📋_Side_Project.py
@@ -7,18 +7,33 @@ import streamlit as st
 
 st.set_page_config(
     page_title='Side Project',
-    page_icon='📋')
+    page_icon='📋',
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
 st.markdown('## Side Project')
 
-st.write(
-    """
-    敘述...
-    """
-)
+tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
 
-st.markdown(
-    '''
+with tab1:
+    st.header("A cat")
+    st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+with tab2:
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+with tab3:
+    st.header("An owl")
+    st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
 
 
-    '''
-)
+col1, col2 = st.columns([1, 2])  # 左欄寬度1，右欄寬度2
+with col1:
+    st.image("./source/photo-stickers.jpg")
+with col2:
+    st.markdown("""
+    ## About Me  
+    Hi, I'm **Ping Chun Wu**, a passionate developer with expertise in:  
+    - Data Science  
+    - Machine Learning  
+    - Software Development  
+    """)
