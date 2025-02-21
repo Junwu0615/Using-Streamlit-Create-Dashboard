e58afd44c9deb3bf7cff55bc760cb4a4806e03cb
jun.audis5 <jun.audis5@gmail.com>
2025-02-21 21:41:49 +0800

    add icon jmeter

--- pages/4_⚙️_DevOps_Concept_Implementation.py
+++ pages/4_⚙️_DevOps_Concept_Implementation.py
@@ -53,10 +53,10 @@ st.markdown(f"""
 |:--|:--|:--|:--|
 | PLAN | 需求與變更管理 | GitHub Projects / Notion | :blue-background[Agile Development] / :blue-background[Defined CI/CD Pipeline] / :blue-background[Version Management] | # Agile 管理開發週期 / 明確定義 CI/CD Pipeline / 版本管理(GitOps)
 | CODE | 版本控制 | GitHub | :blue-background[Git Flow] / PR / Code Quality Management | # Git Flow / PR / 代碼質量管理 ( SonarQube )
-| BUILD | 建置應用程式 | Python / Docker Build | :blue-background[CI Pipeline] / :blue-background[Docker Image] / :blue-background[Docker Hub] |
+| BUILD | 建置應用程式 | Python / Docker | :blue-background[CICD Pipeline] / :blue-background[Docker Build] / Docker Hub |
 | TEST |自動化測試 | Python | :blue-background[Unit Testing] / End-to-End Testing / Service Testing / Security Testing / Load Testing | # 單元測試 / 端對端測試 / 服務測試 / 安全測試 / 負載測試
-| RELEASE | 版本管理與交付 | GitHub Releases / Docker Images | :blue-background[Git Tag Mapping Docker Image Tags] / :blue-background[Automated Release Process] |
-| DEPLOY | 部署 | Docker / Kubernetes | :blue-background[Dev → Staging → Prod] | # 開發環境(Dev) → 測試環境（Staging）→ 正式環境(Prod) 分層部署
+| RELEASE | 版本管理與交付 | GitHub Releases / Docker | :blue-background[Git Tag Mapping Docker Image Tags] / :blue-background[Automated Release Process] |
+| DEPLOY | 部署 | Docker / Kubernetes | :blue-background[Dev　→　Staging　→　Prod] | # 開發環境(Dev)　→　測試環境(Staging)　→　正式環境(Prod) 分層部署
 | OPERATE | 運維與監控 | Kubernetes / Helm | - |
 | MONITOR | 監控與告警 | Prometheus / Grafana / Telegram | :blue-background[Monitor] / :blue-background[Log Management] / :blue-background[Alertmanager] | # 監控 K8S 資源、API、流量 # Loki：集中日誌管理 # Alertmanager：告警機制 (整合 Slack / Telegram)
 
